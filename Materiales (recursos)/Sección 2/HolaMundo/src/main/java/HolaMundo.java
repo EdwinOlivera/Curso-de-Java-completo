7784ecf5a756c2c6eade17e128dbd069928495e2
Edwin Olivera <edwinolivera33@gmail.com>
2021-06-22 09:27:07 -0600

    Avances

---
+++ Materiales (recursos)/Sección 2/HolaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,11 @@
+/*
+Este es un comentarios de varias lineas
+ */
+
+//Comentario de una sola linea
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde java");
+    }
+}
